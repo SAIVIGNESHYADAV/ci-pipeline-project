01f9241422f7558d0b3778f2d6bc41e60f2060b9
Sai Vignesh <pallagorlasai4@gmail.com>
2026-03-30 23:29:32 +0530

    testing failure

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-if __name__ == "__main__":
-    print("Addition:", add(2, 3))
-    print("Subtraction:", sub(5, 2))
+    return a - b   # wrong on purpose
